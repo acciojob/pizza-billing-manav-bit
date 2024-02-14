154e5f4db82191db9fcc2a4951d68ad619251361
MANAV AHUJA <ahujamanav43@gmail.com>
2024-02-14 13:01:58 +0530

    ans

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,11 +4,14 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
-    private String bill;
-
+    private String bill="";
+private boolean addExtraCheese=false;
+    private boolean addExtraToppings=false;
+    private boolean addTakeaway=false;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        price=(isVeg)?300:400;
     }
 
     public int getPrice(){
@@ -17,18 +20,43 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!addExtraCheese){
+            addExtraCheese=true;
+            price+=80;
+            this.bill+="Extra Cheese Added: 80\n";
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!addExtraToppings){
+            addExtraToppings=true;
+            if(!isVeg){
+                price+=120;
+                this.bill+="Extra Toppings Added: 120\n";
+            }
+            else{
+                price+=70;
+                this.bill+="Extra Toppings Added: 70\n";
+            }
+
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!addTakeaway){
+            addTakeaway=true;
+            price+=20;
+
+            this.bill+="Paperbag Added: 20\n";
+
+        }
     }
 
     public String getBill(){
         // your code goes here
+this.bill+=price;
         return this.bill;
     }
 }
